e6ebbd279b2b6a0658141004b0f7902667fcad3a
parmeet-pa <parmeet.ssaini@gmail.com>
2024-10-16 08:51:41 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Parmeet Singh on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
